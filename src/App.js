fbfd3bfb084626cca462b5a3c1784f9aa314db0c
tomkealey <tom.kealey@qlik.com>
2021-12-14 23:58:02 +0100

    add basic onClick card picker

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [category, setCategory] = useState('Press the spacebar');
+  const [colour, setColour] = useState();
+  
+  const categories = [
+    'Life', 'Random','Deep', 'Experiences', 'If you could...', 'Would you rather...'
+  ];
+  
+  const colours = [
+    '#E0E0E0', '#F5F500', '#FF1F1F', '#009DFF', '#21A300', '#6F5F81'
+  ];
+
+  // pick a random card 
+  const getCard = () => {
+    const id = Math.floor(Math.random()*6); 
+
+    setCategory(categories[id]);
+    setColour(colours[id]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p>{category} {colour}</p>
+          <button onClick={() => getCard()}>
+              Click me
+          </button>
       </header>
     </div>
   );
