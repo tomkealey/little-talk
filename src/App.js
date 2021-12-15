69f0936492fae2582bf90048a997f336d5c195d7
tomkealey <tom.kealey@qlik.com>
2021-12-15 22:08:26 +0100

    refactor: use cards object

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ function App() {
   const [category, setCategory] = useState('Press the spacebar');
   const [colour, setColour] = useState();
   
-  const categories = [
-    'Life', 'Random','Deep', 'Experiences', 'If you could...', 'Would you rather...'
-  ];
-  
-  const colours = [
-    '#E0E0E0', '#F5F500', '#FF1F1F', '#009DFF', '#21A300', '#6F5F81'
-  ];
+  const cards = {
+    categories: ['Life', 'Random','Deep', 'Experiences', 'If you could...', 'Would you rather...'],
+    colours: ['#E0E0E0', '#F5F500', '#FF1F1F', '#009DFF', '#21A300', '#6F5F81']
+  }
 
   // pick a random card 
   const getCard = () => {
     const id = Math.floor(Math.random()*6); 
 
-    setCategory(categories[id]);
-    setColour(colours[id]);
+    setCategory(cards.categories[id]);
+    setColour(cards.colours[id]);
   }
 
   return (
