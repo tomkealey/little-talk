a78402d9890f585d5d2054d34b4b6de52b014fb9
Tom Kealey <tom.kealey@qlik.com>
2021-12-16 13:56:23 +0100

    test: add unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders press space bar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Press the spacebar/i);
   expect(linkElement).toBeInTheDocument();
 });
