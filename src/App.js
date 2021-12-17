0a2a156a18439608ffe04f2a335ad307c6b3b4ed
tomkealey <tom.kealey@qlik.com>
2021-12-17 19:55:04 +0100

    feat: add keys

--- src/App.js
+++ src/App.js
@@ -5,9 +5,37 @@ function App() {
   const [category, setCategory] = useState('Press the spacebar');
   const [colour, setColour] = useState();
 
+  const cards = {
+    categories: ['Life', 'Random','Deep', 'Experiences', 'If you could...', 'Would you rather...'],
+    colours: ['#E0E0E0', '#F5F500', '#FF1F1F', '#009DFF', '#21A300', '#6F5F81']
+  }
+
   function handleKeyDown(event) {
-    if(event.code === 'Space') {
-      pickCard();
+    switch(event.code) {
+      case 'Space':
+        // pick a random card
+        pickCard(Math.floor(Math.random()*6));
+        break;
+      case 'KeyL': case 'Digit0':  
+        pickCard(0);
+        break;
+      case 'KeyR': case 'Digit1':  
+        pickCard(1);
+        break;
+      case 'KeyD': case 'Digit2':  
+        pickCard(2);
+        break;
+      case 'KeyE': case 'Digit3':  
+        pickCard(3);
+        break;
+      case 'KeyI': case 'Digit4':  
+        pickCard(4);
+        break;
+      case 'KeyW': case 'Digit5':  
+        pickCard(5);
+        break;
+      default:
+        // do nothing
     }
   }
 
@@ -18,17 +46,10 @@ function App() {
     };
   });
 
-  const cards = {
-    categories: ['Life', 'Random','Deep', 'Experiences', 'If you could...', 'Would you rather...'],
-    colours: ['#E0E0E0', '#F5F500', '#FF1F1F', '#009DFF', '#21A300', '#6F5F81']
-  }
-
-  // pick a random card 
-  const pickCard = () => {
-    const id = Math.floor(Math.random()*6); 
-
-    setCategory(cards.categories[id]);
-    setColour(cards.colours[id]);
+  // pick a card 
+  const pickCard = (x) => {
+    setCategory(cards.categories[x]);
+    setColour(cards.colours[x]);
   }
 
   return (
