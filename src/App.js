a2ccbbec42e8464744ce033064927ead56a9abc4
tomkealey <tom.kealey@qlik.com>
2021-12-18 10:40:39 +0100

    legal: attribute source

--- src/App.js
+++ src/App.js
@@ -68,6 +68,13 @@ function App() {
     pickCard(getRandomNumber());  
   }
 
+  /*!
+   * Get the contrasting color for any hex color
+   * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+   * Derived from work by Brian Suda, https://24ways.org/2010/calculating-color-contrast/
+   * @param  {String} A hexcolor value
+   * @return {String} The contrasting color (black or white)
+   */
   function getContrastYIQ(hexcolor){
     // If a leading # is provided, remove it
     if (hexcolor.slice(0, 1) === '#') {
